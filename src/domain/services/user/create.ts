96f773a390e14dd78d809ba61529076d5fde09f6
Jardesson Eduardo Araújo Mendes <jardesson.eduardo@farmaciasapp.com.br>
2022-07-11 16:00:48 -0500

    feat::sparkles: add create user service

---
+++ src/domain/services/user/create.ts
@@ -0,0 +1,17 @@
+import { UserContracts } from '@domain/models/user/contracts'
+import { UserModel } from '@domain/models/user/models'
+import { CreateUser } from '@domain/repositories/user/store'
+import { infra } from '@infra/common/ioc'
+import { inject } from 'inversify'
+
+export class CreateUserService implements UserContracts.CreateUserService {
+  constructor(
+    @inject(infra.repositories.user.store)
+    private readonly createUserRepository: CreateUser
+  ) {}
+
+  async execute(input: UserContracts.Inputs.ToCreate): Promise<UserModel.Base> {
+    const user = await this.createUserRepository.store(input)
+    return user
+  }
+}
