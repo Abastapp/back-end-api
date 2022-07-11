783145b96a160ab4f9f5d9635503fc7518aa8b87
Jardesson Eduardo Araújo Mendes <jardesson.eduardo@farmaciasapp.com.br>
2022-07-11 16:05:24 -0500

    feat::sparkles: add mongodb schema for user

---
+++ src/infra/helpers/mongodb/schema/user.ts
@@ -0,0 +1,9 @@
+import { Schema } from 'mongoose'
+import { UserModel } from '@domain/models/user/models'
+
+export const userSchema = new Schema<UserModel.Base>({
+  name: { type: 'string', required: true },
+  email: { type: 'string', required: true },
+  password: { type: 'string', required: true },
+  birthDate: { type: 'date', required: true }
+})
