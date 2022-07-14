3bc033d0e21b718268f50f78dd4635d881aec183
Jardesson Eduardo Araújo Mendes <jardesson.eduardo@farmaciasapp.com.br>
2022-07-14 15:16:16 -0500

    feat::sparkles: add types for gas stations

---
+++ src/domain/models/gasStation/contracts.ts
@@ -0,0 +1,15 @@
+import { GasStationModel, Location } from "./models"
+
+export namespace GasStationContracts {
+  export namespace Inputs {
+    export interface ToCreate{
+      name: string
+      email: string
+      cnpj: string
+      location: Location
+    }
+  }
+  export interface CreateGasStationService {
+    execute(input: Inputs.ToCreate): Promise<GasStationModel.Base>
+  }
+}
